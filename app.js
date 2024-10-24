0ebebc4c305004a25408e3fdb447f0b513b9d84c
Antoine Merino Aguilar <antoine.merino@hotmail.com>
2024-10-24 12:54:50 +0200

    corrections

--- app.js
+++ app.js
@@ -11,63 +11,39 @@ form.addEventListener("submit", (event) => {
 
     const usernameLowercase = username.value.toLowerCase();
     if (username.value === usernameLowercase && username.value.length >= 5 && username.value.length <= 10) {
-        username.classList.add("success-input");
-        username.classList.remove("error-input");
-        username.nextElementSibling.classList.add("success");
-        username.nextElementSibling.classList.remove("error");
+        username.parentElement.classList = "success";
         username.nextElementSibling.innerText = "Valid username.";
     } else {
         isValid = false;
-        username.classList.add("error-input");
-        username.classList.remove("success-input");
-        username.nextElementSibling.classList.add("error");
-        username.nextElementSibling.classList.remove("success");
+        username.parentElement.classList = "error";
         username.nextElementSibling.innerText = "Le username doit contenir entre 5 et 10 caractères minuscules.";
     }
 
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (emailRegex.test(email.value)) {
-        email.classList.add("success-input");
-        email.classList.remove("error-input");
-        email.nextElementSibling.classList.add("success");
-        email.nextElementSibling.classList.remove('error');
+        email.parentElement.classList = "success";
         email.nextElementSibling.textContent = 'Valid email.';
     } else {
         isValid = false;
-        email.classList.add("error-input");
-        email.classList.remove("success-input");
-        email.nextElementSibling.classList.add('error');
-        email.nextElementSibling.classList.remove('success');
+        email.parentElement.classList = "error";
         email.nextElementSibling.textContent = 'Invalid email address.';
     }
 
     if (password.value.length >= 8 && password.value.length <= 15) {
-        password.classList.add("success-input");
-        password.classList.remove("error-input");
-        password.nextElementSibling.classList.add("success");
-        password.nextElementSibling.classList.remove("error");
+        password.parentElement.classList = "success";
         password.nextElementSibling.innerText = "Valid password.";
     } else {
         isValid = false;
-        password.classList.add("error-input");
-        password.classList.remove("success-input");
-        password.nextElementSibling.classList.add("error");
-        password.nextElementSibling.classList.remove("success");
+        password.parentElement.classList = "error";
         password.nextElementSibling.innerText = "Le password doit contenir entre 8 et 15 caractères.";
     }
 
     if (passwordConfirmation.value === password.value && passwordConfirmation.value.length >= 8 && passwordConfirmation.value.length <= 15) {
-        passwordConfirmation.classList.add("success-input");
-        passwordConfirmation.classList.remove("error-input");
-        passwordConfirmation.nextElementSibling.classList.add("success");
-        passwordConfirmation.nextElementSibling.classList.remove("error");
+        passwordConfirmation.parentElement.classList = "success";
         passwordConfirmation.nextElementSibling.innerText = "Valid confirmation password.";
     } else {
         isValid = false;
-        passwordConfirmation.classList.add("error-input");
-        passwordConfirmation.classList.remove("success-input");
-        passwordConfirmation.nextElementSibling.classList.add("error");
-        passwordConfirmation.nextElementSibling.classList.remove("success");
+        passwordConfirmation.parentElement.classList = "error";
         passwordConfirmation.nextElementSibling.innerText = "Les passwords doivent être identiques.";
     }
 
